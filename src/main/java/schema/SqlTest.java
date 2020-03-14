5d1be8dfbbff5876234e8584b4106b459877f515
ant <1596863112@qq.com>
2020-03-14 13:53:17 +0800

    ant

--- src/main/java/schema/SqlTest.java
+++ src/main/java/schema/SqlTest.java
@@ -5,17 +5,15 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
-import org.hibernate.SessionFactory;
 import pojo.videos.Video;
 import schema.entity.VideoInfo;
 import schema.mapper.VideoInfoDao;
-
 import java.io.InputStream;
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class SqlTest {
 
@@ -32,10 +30,7 @@ public class SqlTest {
 
     public static void main(String[] args) {
         try {
-
-
-                    insert();
-
+            insert();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -45,53 +40,54 @@ public class SqlTest {
         SqlSessionFactory sqlSessionFactory=getSqlSessionFactory();
         final SqlSession sqlSession = sqlSessionFactory.openSession();
         final VideoInfoDao mapper = sqlSession.getMapper(VideoInfoDao.class);
-        Executors.newFixedThreadPool(7)
-                .submit(()->{
-                    for (int i = 0; i < 2899; i++) {
+        final ExecutorService executorService = Executors.newFixedThreadPool(7);
 
-                        List<Video> list = null;
-                        try {
-                            list = ApiServiceFactory
-                                    .getService()
-                                    .getVideosOfLimit(i, 50)
-                                    .getResponse()
-                                    .getVideos();
-                        } catch (Exception e) {
-                            e.printStackTrace();
+        for (int i = 0; i < 2899; i++) {
+            int finalI = i;
+            AtomicBoolean flag= new AtomicBoolean(true);
+            executorService.submit(()->{
+                    List<Video> list = ApiServiceFactory
+                            .getService()
+                            .getVideosOfLimit(finalI, 50)
+                            .getResponse()
+                            .getVideos();
+
+                    if(!list.isEmpty()){
+                        for (Video video : list) {
+                            VideoInfo videoInfo = new VideoInfo();
+                            videoInfo.setVid(video.getVid())
+                                    .setUid(video.getUid())
+                                    .setAddtime(new Date(video.getAddtime()))
+                                    .setChannel(video.getChannel())
+                                    .setDislikes(video.getDislikes())
+                                    .setEmbeddedUrl(video.getEmbedded_url())
+                                    .setDuration(video.getDuration())
+                                    .setFramerate(video.getFramerate())
+                                    .setKeyword(video.getKeyword())
+                                    .setPreviewVideoUrl(video.getPreview_video_url())
+                                    .setLikes(video.getLikes())
+                                    .setViewnumber(video.getViewnumber())
+                                    .setHd(video.isHd())
+                                    .setPrivatee(video.isPrivatee())
+                                    .setTitle(video.getTitle())
+                                    .setPriviewUrl(video.getPreview_url())
+                                    .setVideoUrl(video.getVideo_url());
+                            mapper.insertOne(videoInfo);
+                            sqlSession.commit(true);
                         }
 
-                        if(!list.isEmpty()){
-                            List<VideoInfo> list1=new ArrayList<>();
-                            for (Video video : list) {
-                                VideoInfo videoInfo = new VideoInfo();
-                                videoInfo.setVid(video.getVid())
-                                        .setUid(video.getUid())
-                                        .setAddtime(new Date(video.getAddtime()))
-                                        .setChannel(video.getChannel())
-                                        .setDislikes(video.getDislikes())
-                                        .setEmbeddedUrl(video.getEmbedded_url())
-                                        .setDuration(video.getDuration())
-                                        .setFramerate(video.getFramerate())
-                                        .setKeyword(video.getKeyword())
-                                        .setPreviewVideoUrl(video.getPreview_video_url())
-                                        .setLikes(video.getLikes())
-                                        .setViewnumber(video.getViewnumber())
-                                        .setHd(video.isHd())
-                                        .setPrivatee(video.isPrivatee())
-                                        .setTitle(video.getTitle())
-                                        .setPriviewUrl(video.getPreview_url())
-                                        .setVideoUrl(video.getVideo_url());
-                                list1.add(videoInfo);
-                            }
-                            mapper.batchInsert(list1);
-                        }
-
-                        sqlSession.commit(true);
-                        sqlSession.clearCache();
-                        System.out.println("页：" + i);
-
+                    }else {
+                        flag.set(false);
                     }
                 });
+            if (!flag.get()){
+                continue;
+            }else {
+                System.out.println("页：" + finalI);
+            }
+
+
+        }
     }
 
 }
